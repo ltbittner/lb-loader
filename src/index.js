3049aba054941a7a93163a79e0b3098c098131ad
Justin Jung <justin.jung@thinkingbox.ca>
2016-05-26 22:51:32 -0700

    refactoring of index.js completed

--- src/index.js
+++ src/index.js
@@ -1,162 +1,134 @@
 let instance = null;
 
 class LBLoader {
-  constructor(args = null) {
+  constructor(args = {}) {
     if(instance) {
       return instance;
     }
     instance = this;
 
-    if(args.preload){
+    if(args.preload) {
       this.preloadQueue = [];
-      this.preloadCount = 0;
       this.preloadProgressCallback = args.preloadProgressCallback || null;
-      this.preloadCompletedCallback = args.preloadCompletedCallback || null;
+      this.preloadCompletedCallback = () => {
+        if(args.preloadCompletedCallback) {
+          args.preloadCompletedCallback();
+        }
+        if(args.autoStartBackgroundLoad === true) {
+          this.startBackgroundLoad();
+        }
+        if(!args.backgroundLoad) {
+          this.destroy();
+        }
+      };
       this.initializeQueue(args.preload, this.preloadQueue);
     }
-    
+
     if(args.backgroundLoad) {
       this.backgroundLoadQueue = [];
-      this.backgroundLoadCount = 0;
-      this.backgroundProgressCallback = args.backgroundProgressCallback || null;
-      this.backgroundCompletedCallback = args.backgroundCompletedCallback || null;
-      this.autoStartBackgroundLoad = false || args.autoStartBackgroundLoad;
+      this.backgroundLoadProgressCallback = args.backgroundLoadProgressCallback || null;
+      this.backgroundLoadCompletedCallback = () => {
+        if(args.backgroundLoadCompletedCallback) {
+          args.backgroundLoadCompletedCallback();
+        }
+        this.destroy();
+      };
       this.initializeQueue(args.backgroundLoad, this.backgroundLoadQueue);
     }
   }
 
   startPreload() {
-    if(this.preloadQueue) {
-      this.load(this.preloadQueue, this.preloadCount, this.preloadProgressCallback);
+    if(!this.preloadQueue) {
+      console.error(new Error('Assets to be preloaded are not defined.'));
+      return;
     }
+    this.loadAssets(this.preloadQueue, this.preloadProgressCallback, this.preloadCompletedCallback);
+  }
+
+  startBackgroundLoad() {
+    if(!this.backgroundLoadQueue) {
+      console.error(new Error('Assets to be loaded in the background are not defined.'));
+      return;
+    }
+    this.loadAssets(this.backgroundLoadQueue, this.backgroundLoadProgressCallback, this.backgroundLoadCompletedCallback);
   }
 
   initializeQueue(args, queue) {
-    if(args == null || !args){
-      return;
-    }
-    for(let asset of args) {
-      if((/\.(gif|jpg|jpeg|tiff|png)$/i).test(asset)) {
-        queue.push({
-          type: 'image',
-          src: asset,
-        });
-      } else if((/\.(mp4|webm)$/i).test(asset)) {
-        queue.push({
-          type: 'video',
-          src: asset
-        });
-      } else if((/\.(mp3|ogg|wav)$/i).test(asset)) {
-        queue.push({
-          type: 'audio',
-          src: asset
-        });
+    for(let src of args) {
+      let type;
+      if((/\.(gif|jpg|jpeg|tiff|png)$/i).test(src)) {
+        type = 'image';
+      } else if((/\.(mp3|ogg|wav)$/i).test(src)) {
+        type = 'audio';
+      } else if((/\.(mp4|webm)$/i).test(src)) {
+        type = 'video';
+      }
+      if(type) {
+        queue.push({ type, src });
+      } else {
+        this.destroy();
+        console.error(new Error('Unable to handle \'' + src + '\'. File format is not supported.'));
+        return;
       }
     }
   }
 
-  load(queue, count, handler) {
+  loadAssets(queue, progressCallback, completeCallback) {
+    let count = 0;
     for(let asset of queue) {
-      var t = this.loadAsset(asset.src, asset.type);
-      t.then(() => {
-        handler(queue, count);
-        // this.progressHandler(queue, count)
-      })
-      .catch(() => {
+      this.loadAsset(asset.src, asset.type).then(() => {
+        count++;
+        if(progressCallback) {
+          this.progressHandler(queue, count, progressCallback);  
+        }
+        if(count == queue.length) {
+          completeCallback();
+        }
+      }).catch(() => {
         this.errorHandler(asset.src);
       });
     }
   }
 
-  loadAsset(path, type) {
+  loadAsset(src, type) {
     return new Promise((resolve, reject) => {
       let asset;
       if(type == 'image') {
         asset = new Image();
-      } else if(type == 'video') {
-        asset = document.createElement('video');
+      } else {
+        asset = document.createElement(type);
         asset.addEventListener('suspend', resolve);
       }
-      else if(type == 'audio') {
-        asset = document.createElement('audio');
-        asset.addEventListener('suspend', resolve);
-      }
-
       asset.onload = resolve;
       asset.onerror = reject;
-      asset.src = path + "?_=" + (new Date().getTime());
-      if(type == 'video' || type == 'audio') {
+      asset.src = src;
+      if(type == 'video') {
         asset.load();
       }
     });
   }
 
-  progressHandler(queue, count, handler) {
-    count++;
- 
-    if(this.preloadProgressCallback) {
-      const data = {
-        completed: count,
-        total: queue.length,
-        percentage: Math.round(((count / queue.length) * 100))
-      };
-      // this.preloadProgressCallback(data);
-      handler(data);
-    }
-
-    if(count == queue.length) {
-      this.preloadCompletedHandler();
-    }
+  progressHandler(queue, count, progressCallback) {
+    progressCallback({
+      completed: count,
+      total: queue.length,
+      percentage: Math.round(((count / queue.length) * 100))
+    });
   }
 
   errorHandler(src) {
-    this.resetAllCallbacks();
-    this.preloadQueue = null;
-    this.backgroundQueue = null;
-    throw "Cannot find file " + src;
+    this.destroy();
+    console.error(new Error('\'' + src + '\' is not found.'));
   }
 
-
-
-
-  backgroundProgressHandler() {
-    this.numberOfBackgroundAssets++;
-g    if(this.backgroundProgressCallback) {
-      let data = {
-        completed: this.numberOfBackgroundAssets,
-        total: this.totalNumberOfBackgroundAssets,
-        percentage: Math.round(((this.numberOfBackgroundAssets / this.totalNumberOfBackgroundAssets) * 100))
-      };
-      this.backgroundProgressCallback(data);
-    }
-
-    if(this.numberOfBackgroundAssets == this.totalNumberOfBackgroundAssets) {
-      this.backgroundCompletedHandler();
-    }
-  }
-
-  preloadCompletedHandler() {
-    if(this.preloadCompletedCallback) {
-      this.preloadCompletedCallback();
-    }
-    if(this.autoStartBackgroundLoad) {
-      this.startBackgroundLoad();
-    }
-  }
-
-  backgroundCompletedHandler() {
-    if(this.backgroundCompletedCallback) {
-      this.backgroundCompletedCallback();
-    }
-    this.resetAllCallbacks();
-  }
-
-  resetAllCallbacks() {
+  destroy() {
+    this.preloadQueue = [];
     this.preloadProgressCallback =  null;
     this.preloadCompletedCallback =  null;
-
+    this.backgroundLoadQueue = [];
     this.backgroundProgressCallback = null;
     this.backgroundCompletedCallback = null;
+    instance = null;
   }
 }
 
